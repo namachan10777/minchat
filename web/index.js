4763196073b33cbd986f1336346e5e4327c5bc6a
namachan10777 <namachan10777@gmail.com>
2018-12-14 16:19:01 +0900

    web

---
+++ web/index.js
@@ -0,0 +1,55 @@
+//let serverURL = 'http://d-kitamura.sakura.ne.jp/lecture/ws-server';
+let serverURL = 'ws://localhost:8080';
+let socket = new WebSocket(serverURL);
+
+let timelineNode = document.getElementById('timeline');
+let msgboxNode = document.getElementById('msgbox');
+
+let username = 'guest';
+
+socket.addEventListener('open', e => {
+  socket.send(JSON.stringify({
+    type: 'request-history',
+    times: 10
+  }));
+});
+
+function appendTL(msg) {
+  let elm = document.createElement('div');
+  let username = document.createElement('div');
+  let maincontent = document.createElement('div');
+
+  username.textContent = msg.username;
+  maincontent.textContent = msg.text;
+
+  elm.appendChild(username);
+  elm.appendChild(maincontent);
+
+  timelineNode.appendChild(elm);
+}
+
+socket.addEventListener('message', e => {
+  if (e.target.value.type === 'msgs') {
+    for (let msg in e.target.value.msgs) {
+      appendTL(msg);
+    }
+  }
+});
+
+let postButton    = document.getElementById('post-button');
+let textArea      = document.getElementById('maintext');
+let usernameInput = document.getElementById('username');
+
+function postMsg() {
+  socket.send({
+    type: 'post',
+    msg: {
+      username: usernameInput.text,
+      text: textArea.text
+    }
+  });
+}
+
+postButton.addEventListener('onclick', e => {
+  postMsg();
+});

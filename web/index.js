a396ed310b0bb91638dd54fcba309a8b0e3448e9
namachan10777 <namachan10777@gmail.com>
2018-12-21 12:51:24 +0900

    disable join forms before websocket handshaking

--- web/index.js
+++ web/index.js
@@ -3,6 +3,23 @@ let serverURL = 'ws://localhost:8080';
 let socket = new WebSocket(serverURL);
 
 let root = document.getElementById("root");
+socket.onopen = () => {
+  socket.send(JSON.stringify({
+    req: 'user-list'
+  }));
+}
+
+let userList = undefined;
+function updateUserList(list) {
+  userList = list;
+  document.querySelectorAll('.join-forms').forEach(elm => elm.disabled = false);
+}
+
+socket.onmessage = (e) => {
+  let msg = JSON.parse(e.data);
+  if (msg.path === '/userlist')
+    updateUserList(msg.content);
+}
 
 function UsernameInput () {
   let root = document.createElement("div");
@@ -12,6 +29,8 @@ function UsernameInput () {
   usernameInput.onChange = (e) => {
   };
   usernameInput.classList.add("username-input");
+  usernameInput.classList.add("join-forms");
+  usernameInput.disabled = userList === undefined;
 
   let usernameInputUnderline = document.createElement("div");
   usernameInputUnderline.classList.add("username-input-underline");
@@ -29,6 +48,8 @@ function JoinWindow () {
   let joinButton = document.createElement("button");
   joinButton.textContent = "Join!";
   joinButton.classList.add("join-button");
+  joinButton.classList.add("join-forms");
+  joinButton.disabled = userList === undefined;
 
   screen.appendChild(UsernameInput());
   screen.appendChild(joinButton);
